7050c40fa5ff70628467713f5ce08892048b0f17
Jamie Hall <the.sly.marbo@googlemail.com>
2015-07-20 16:35:26 +0100

    Fixed time format tests

--- time_test.go
+++ time_test.go
@@ -8,12 +8,13 @@ import (
 const customLayout = "2006-01-02T15:04Z07:00"
 
 var (
-	timeVal = time.Date(2015, 7, 1, 9, 27, 0, 0, time.UTC)
+	timeVal         = time.Date(2015, 7, 1, 9, 27, 0, 0, time.UTC)
+	originalLayouts = TimeLayouts
 )
 
 func TestParseTimeUsingOnlyDefaultLayouts(t *testing.T) {
 	// Positive cases
-	for _, layout := range DefaultTimeLayouts() {
+	for _, layout := range originalLayouts {
 		s := timeVal.Format(layout)
 		if tv, err := parseTime(s); err != nil || !tv.Equal(timeVal) {
 			t.Error("expected no err and times to equal, got err %v and time value %v", err, tv)
@@ -34,19 +35,19 @@ func TestParseTimeUsingOnlyDefaultLayouts(t *testing.T) {
 }
 
 func TestParseTimeUsingCustomLayoutsPrepended(t *testing.T) {
-	TimeLayouts = append([]string{customLayout}, DefaultTimeLayouts()...)
+	TimeLayouts = append([]string{customLayout}, originalLayouts...)
 	custom := timeVal.Format(customLayout)
 	if tv, err := parseTime(custom); err != nil || !tv.Equal(timeVal) {
 		t.Error("expected no err and times to equal, got err %v and time value %v", err, tv)
 	}
-	TimeLayouts = DefaultTimeLayouts()
+	TimeLayouts = originalLayouts
 }
 
 func TestParseTimeUsingCustomLayoutsAppended(t *testing.T) {
-	TimeLayouts = append(DefaultTimeLayouts(), customLayout)
+	TimeLayouts = append(originalLayouts, customLayout)
 	custom := timeVal.Format(customLayout)
 	if tv, err := parseTime(custom); err != nil || !tv.Equal(timeVal) {
 		t.Error("expected no err and times to equal, got err %v and time value %v", err, tv)
 	}
-	TimeLayouts = DefaultTimeLayouts()
+	TimeLayouts = originalLayouts
 }

224b4ae47c6bc86926a6fa27b17fa64a17fba195
Jamie Hall <the.sly.marbo@googlemail.com>
2015-09-22 19:31:17 +0100

    Fixed race condition in database testing and neatened code
    The test was failing because the channel op was occurring before
    making the change to known. Since the channel op causes a schedule,
    the goroutine was being scheduled without having changed known. We
    now change known first.
    
    While I was at it, I did some small refactoring to make db more
    self-contained and to remove NewDB, which didn't need to be there
    in the first place.
    
    All this needs to be tidied up and documented at some point ¬.¬

--- database.go
+++ database.go
@@ -4,7 +4,10 @@ var database *db
 var disabled bool
 
 func init() {
-	database = NewDB()
+	database = new(db)
+	database.req = make(chan string)
+	database.res = make(chan bool)
+	database.known = make(map[string]struct{})
 	go database.Run()
 }
 
@@ -15,25 +18,14 @@ type db struct {
 }
 
 func (d *db) Run() {
-	d.known = make(map[string]struct{})
-	var s string
-
-	for {
-		s = <-d.req
+	for s := range d.req {
 		if disabled {
 			d.res <- false
 		} else if _, ok := d.known[s]; ok {
 			d.res <- true
 		} else {
-			d.res <- false
 			d.known[s] = struct{}{}
+			d.res <- false
 		}
 	}
-}
-
-func NewDB() *db {
-	out := new(db)
-	out.req = make(chan string)
-	out.res = make(chan bool)
-	return out
 }

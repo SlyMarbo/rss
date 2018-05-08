bbde65d3b9d3e67c0dba33b68dca8096aa0bff2d
Matteo Kloiber <info@matt3o12.de>
2018-05-08 14:57:09 +0200

    parseTime: text timezones are now parsed correctly
    If a format contains the timezone as text,
    such as 'Jan 2, 2006 15:04 PM MST',
    golang does not automacitally load the timezone
    offset. This has to be done manually through
    LoadTimezone. The variable
    `TimeLayoutsLoadLocation` contains all parse
    formats with such a timezone. parseTime now
    loads the timezone (if possible) and returns
    the time in the correct timezone.
    
    Tests have also been added to test more time
    formats. TimeLayouts have also been added
    for date that contain both the texual timezone
    and offset (such as Mon Jan _2 15:04:05 MST
    -0200). In this case, parseTime always uses
    the offset, even if it conflicts with the
    known offset because it can be ambiguous:
    https://news.ycombinator.com/item?id=10199812

--- time_test.go
+++ time_test.go
@@ -64,3 +64,46 @@ func TestParseWithTwoDigitYear(t *testing.T) {
 		t.Errorf("expected no err and year to be 2016, got err %v, and year %v", err, tv.Year())
 	}
 }
+
+// TestParseTime tests parseTime against some
+// common and some not so valid time formts.
+// Feel free to add more by adding another slice
+// to the tests
+func TestParseTime(t *testing.T) {
+	tests := []struct {
+		in       string
+		expected time.Time
+	}{{
+		"Sun, 06 Sep 2009 16:20:00 +0000",
+		time.Date(2009, 9, 6, 16, 20, 0, 0, time.UTC),
+	}, {
+		"Sun, 06 Sep 2009 16:20:00 -0300",
+		time.Date(2009, 9, 6, 19, 20, 0, 0, time.UTC),
+	}, {
+		"06 Sep 2009 16:18:00 EST",
+		time.Date(2009, 9, 6, 21, 18, 0, 0, time.UTC),
+	}, {
+		"Sun, 06 Sep 2009 16:18:00 EST",
+		time.Date(2009, 9, 6, 21, 18, 0, 0, time.UTC),
+	}, {
+		"Sun, 06 Sep 2010 16:43:59 EST +0100", // ignore EST
+		time.Date(2010, 9, 6, 15, 43, 59, 0, time.UTC),
+	}, {
+		"Sun, 06 Sep 2009 16:18:00 +0200 EST", // ignore EST
+		time.Date(2009, 9, 6, 14, 18, 0, 0, time.UTC),
+	}, {
+		"Sun, 06 Sep 2009 16:18:00 +0200",
+		time.Date(2009, 9, 6, 14, 18, 0, 0, time.UTC),
+	}}
+
+	for _, c := range tests {
+		time, err := parseTime(c.in)
+		if err != nil {
+			t.Errorf("%q Date is not valid: %q.", c.in, err)
+		}
+
+		if d := time.UTC(); !d.Equal(c.expected) {
+			t.Errorf("%q: %q != %q.", c.in, c.expected, d)
+		}
+	}
+}
